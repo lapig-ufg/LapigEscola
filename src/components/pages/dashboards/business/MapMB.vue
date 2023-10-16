dd345f2f398e8a4cd0109dec4dc91a8a484fef3a
Jairo Matos da Rocha <devjairomr@gmail.com>
2023-10-15 22:18:25 -0300

    temabase

--- src/components/pages/dashboards/business/MapMB.vue
+++ src/components/pages/dashboards/business/MapMB.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div >
     <div ref="map" class="map"></div>
   </div>
 </template>
@@ -11,41 +11,104 @@ import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import Overlay from 'ol/Overlay';
+import TileWMS from 'ol/source/TileWMS';
 import { fromLonLat } from 'ol/proj';
+import { Icon, Style } from 'ol/style';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
 
 export default {
-  mounted() {
-    this.map = new Map({
-      target: this.$refs.map,
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-      ],
-      view: new View({
-        center: fromLonLat([0, 0]),
-        zoom: 2,
-      }),
-    });
+    mounted() {
+        const brazilCenter = fromLonLat([-47.687434,-14.130147]); // Coordenadas do centro do Brasil
 
-    // Adicione um marcador (overlay)
-    const marker = new Overlay({
-      position: fromLonLat([0, 0]),
-      element: document.createElement('div'),
-    });
-    this.map.addOverlay(marker);
+        this.map = new Map({
+            target: this.$refs.map,
+            layers: [
+                new TileLayer({
+                    source: new OSM(),
+                }),
+                new TileLayer({
+                    source: new TileWMS({
+                        url: 'https://geoservicos.ibge.gov.br/geoserver/BDIA/wms',
+                        params: {
+                            'LAYERS': 'BDIA:gpc_geol_bioma',
+                            'cql_filter': 'cd_bioma=3',
+                            'FORMAT': 'image/png',
+                            'TRANSPARENT': true,
+                        },
+                    }),
+                }),
+            ],
+            view: new View({
+                center: brazilCenter,
+                zoom: 4, // Ajuste o nível de zoom conforme necessário
+            }),
+        });
 
-    // Abra uma popup quando o marcador é clicado
-    marker.getElement().addEventListener('click', () => {
-      alert('Clique no marcador!');
-    });
-  },
+        const iconStyle = new Style({
+            image: new Icon({
+                anchor: [0, 0],
+                src: '/images/icons/map/point.svg', // Substitua pelo caminho da sua imagem de ícone
+                size: [32, 32], // Substitua pelas dimensões reais do ícone
+            }),
+        });
+
+
+        const markerElement = document.createElement('div');
+        markerElement.classList.add('fas', 'fa-map-marker-alt', 'gcl-map-pin');
+        markerElement.style.width = '32px';
+        markerElement.style.height =  '32px';
+        // Adicione um marcador (overlay)
+
+        const marker = new Overlay({
+            position: brazilCenter,
+            element: markerElement
+        });
+
+        this.map.addOverlay(marker);
+
+        // Crie um ícone para o marcador
+        const markerIcon = new Feature({
+            geometry: new Point(brazilCenter),
+        });
+
+        markerIcon.setStyle(
+            iconStyle
+        );
+
+        marker.getElement().addEventListener('click', () => {
+            this.onMarkerClick(); // Chame um método personalizado quando o marcador é clicado
+        });
+
+        // Adicione o marcador ao mapa
+        this.map.addLayer(new VectorLayer({
+            source: new VectorSource({
+                features: [markerIcon],
+            }),
+        }));
+
+        // Abra uma popup quando o marcador é clicado
+
+    },
+    methods: {
+        onMarkerClick() {
+            console.log('Clique no marcador!');
+            alert('Clique no marcador!');
+        },
+    },
 };
 </script>
 
-<style scoped>
+
+<style lang="scss">
+.gcl-map-pin{
+  font-size: 32px;
+}
 .map {
   width: 100%;
-  height: 400px;
+  height: 800px;
+
 }
 </style>
