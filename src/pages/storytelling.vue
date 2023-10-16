c2af5604b94f424d06bcad4974f3e3201a6cf6a6
Jairo Matos da Rocha <devjairomr@gmail.com>
2023-10-16 11:07:15 -0300

    dev

---
+++ src/pages/storytelling.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+import { useViewWrapper } from '/@src/stores/viewWrapper'
+
+const viewWrapper = useViewWrapper()
+viewWrapper.setPageTitle('Blank Page 1')
+
+useHead({
+  title: 'Blank Page 1 - Sidebar - Vuero',
+})
+</script>
+
+<template>
+  <SidebarLayout>
+    <!-- Content Wrapper -->
+    <div class="page-content-inner">
+
+    </div>
+
+    <Conteudo>
+      <template #header>
+        <div class="content-section-header">
+        </div>
+      </template>
+    </Conteudo>
+  </SidebarLayout>
+</template>
